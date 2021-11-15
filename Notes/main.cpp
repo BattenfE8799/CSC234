9651e6c22741c9b84bcb73b7a9ee18df6b658e77
Elizabeth Battenfield <71232235+BattenfE8799@users.noreply.github.com>
2021-11-14 23:17:30 -0500

    worked

---
+++ Notes/main.cpp
@@ -0,0 +1,50 @@
+// Notes for doing C++
+/* multiline
+comments
+*/
+#include <iostream>
+#include <cmath> //math library from the C language library
+#include <cstdlib> //math functinos for integers from cstdlib library
+
+using namespace std; //makes it so you dont have to put st::cout for each cout line
+
+int main()
+{
+    int object; //declare variables first and always end with ;
+    object = "object"; //initializing variable
+
+    cout << "Hello world!" << object << endl; //displays text Hello World, object, and ends in a new line
+    //newline = endl or "\n"
+    //whitespace = blank spaces or tabs between items within a statement and between statements(newlines).
+    cin >> userInput; //gets user input
+
+
+
+    return 0;
+
+
+}
+void/*<defines value of return statement */ functions()
+{
+    //function is a named list of statements
+
+
+    return 0; //return types may be int, char, double, or other types
+
+} //FUNCTION DEFINITION: new functions name, and a blck of statments ie. double name(){ block of statements}
+
+
+double MilesToLaps(double userMiles){
+   double userLaps;
+
+   userLaps = (userMiles / 0.25)
+   return userLaps
+
+
+int main() {
+   double userMiles;
+   cin >> "Enter number of miles: ";
+   double userLaps = MilesToLaps(userMiles);
+   cout << fixed << setprecision(2);
+
+
